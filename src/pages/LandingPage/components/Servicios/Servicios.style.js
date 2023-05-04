795a6eace5db8ec40a2456ba6356064ceab17119
Laura López Alonso <109240574+lauralopezalo@users.noreply.github.com>
2023-05-04 20:01:38 +0200

    feat: create services section structure

---
+++ src/pages/LandingPage/components/Servicios/Servicios.style.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const IconContainer = styled.div`
+    width: 50px;
+    height: 50px;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    background-color: #d6bcfa;
+    color: #6f00ff;
+    margin-bottom: 4px;
+`;
+
+export const ServiceContainer = styled.div`
+    border: 1px solid #CBD5E0;
+    padding: 1.5rem;
+    border-radius: 0.5rem;
+`;
+
+
