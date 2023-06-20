89e95b0122c7a7a876b3ba6cde0dc3973d471679
Marina <72014542+marinosrt@users.noreply.github.com>
2023-06-20 20:33:54 +0200

    Add files via upload

---
+++ Tech4Good_Group10_V1-1/src/main/java/Tech4Good_Group10/Tech4GoodGroup10Application.java
@@ -0,0 +1,15 @@
+package Tech4Good_Group10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan
+public class Tech4GoodGroup10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tech4GoodGroup10Application.class, args);
+	}
+
+}
